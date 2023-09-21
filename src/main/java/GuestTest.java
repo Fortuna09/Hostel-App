941c634996559d0d5b43d444a798414955dffcfd
Fortuna <rafael.fortuna@sou.unifal-mg.edu.br>
2023-09-21 16:51:22 -0300

    fisrt

---
+++ src/main/java/GuestTest.java
@@ -0,0 +1,118 @@
+import hostelapp.model.*;
+import hostelapp.model.Date;
+
+import java.util.*;
+
+public class GuestTest {
+
+    public static void main(String[] args) {
+
+        Hostel hostel = Hostel.getHostel();
+        hostel.setName("Sparkling Water");
+        hostel.setEmail("sparkling@gmail.com");
+        hostel.setPhone("+(55)-35-3343-1234");
+
+        Address hostelAddress = new Address();
+        hostelAddress.setAddress("Rua Camilo Soares");
+        hostelAddress.setCity("Caxambu");
+        hostelAddress.setState("MG");
+        hostelAddress.setCountry("Brazil");
+        hostelAddress.setZipCode("123400-000");
+
+        hostel.setAddress(hostelAddress);
+
+        System.out.println("Printing hostel details:");
+        System.out.println(hostel.toString());
+        System.out.println();
+
+        Guest florentino = new Guest();
+        florentino.setTitle(Title.MS);
+        florentino.setName("Florentino");
+        florentino.setLastName("Ariza");
+        florentino.setEmail("ariza@gmail.com");
+
+        Date birthday = new Date();
+        birthday.setDay(35);
+        birthday.setMonth(2);
+        birthday.setYear(1970);
+
+        florentino.setBirthday(birthday);
+
+        Address address = new Address();
+        address.setAddress("Rua Joaquim Lázaro Gomes");
+        address.setCity("Alfenas");
+        address.setZipCode("37130-000");
+        address.setState("MG");
+        address.setCountry("Brazil");
+
+        florentino.setAddress(address);
+
+
+        Reservation reservation1 = new Reservation();
+        Date reservationDate1 = new Date(12, 5, 2023);
+        Date checkinDate1 = new Date(15, 7, 2023);
+        Date checkoutDate1 = new Date(22, 7, 2023);
+        reservation1.setReservationDate(reservationDate1);
+        reservation1.setCheckinDate(checkinDate1);
+        reservation1.setCheckoutDate(checkoutDate1);
+
+        Reservation reservation2 = new Reservation();
+        Date reservationDate2 = new Date(10, 2, 2023);
+        Date checkinDate2 = new Date(20, 8, 2023);
+        Date checkoutDate2 = new Date(23, 8, 2023);
+        reservation2.setReservationDate(reservationDate2);
+        reservation2.setCheckinDate(checkinDate2);
+        reservation2.setCheckoutDate(checkoutDate2);
+
+//        ArrayList<Reservation> reservations = new ArrayList<>();
+//        reservations.add(reservation1);
+//        reservations.add(reservation2);
+
+//        florentino.setReservations(reservations);
+
+        florentino.addReservation(reservation1);
+        florentino.addReservation(reservation2);
+
+        Address florentinoAddress = florentino.getAddress();
+        String city = florentinoAddress.getCity();
+
+        System.out.println("Title...: " + florentino.getTitle());
+        System.out.println("Name...: " + florentino.getName());
+        System.out.println("Last name...: " + florentino.getLastName());
+        System.out.println("Email...: " + florentino.getEmail());
+        System.out.println("Birthdate..: " + florentino.getBirthday());
+        System.out.println("Address...: " + florentino.getAddress().getAddress());
+        System.out.println("City....: " + city);
+        System.out.println("State...: " + florentino.getAddress().getState());
+        System.out.println("Zip Code..: " + florentino.getAddress().getZipCode());
+        System.out.println("Country..: " + florentino.getAddress().getCountry());
+
+        List<Reservation> temp = florentino.getReservations();
+        for (Reservation reservation: temp){
+            System.out.println(reservation);
+        }
+
+
+        Guest fermina = new Guest();
+        fermina.setName("Fermina");
+        fermina.setLastName("Daza");
+
+        Guest atticus = new Guest("Atticus", "Finch");
+
+        Guest gregor = new Guest("Gregor");
+        gregor.setLastName("Samsa");
+
+        System.out.println();
+        System.out.println("Name...: " + fermina.getName());
+        System.out.println("Last name...: " + fermina.getLastName());
+
+        System.out.println();
+        System.out.println("Name...: " + atticus.getName());
+        System.out.println("Last name...: " + atticus.getLastName());
+
+        System.out.println();
+        System.out.println("Name...: " + gregor.getName());
+        System.out.println("Last name...: " + gregor.getLastName());
+
+    }
+}
---
+++ src/main/java/hostelapp/model/Guest.java
@@ -0,0 +1,94 @@
+package hostelapp.model;
+
+import java.util.*;
+
+public class Guest {
+    private String name;
+    private String lastName;
+
+    private Address address;
+
+    private String email;
+
+    private Date birthday;
+
+    private List<Reservation> reservations = new ArrayList<>();
+
+    private Title title;
+
+    public Guest() {
+        this("");
+    }
+    public Guest(String name){
+        this(name, "");
+    }
+
+    public Guest(String name, String lastName){
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean setName(String name) {
+        if (name.length() > 2) {
+            this.name = name;
+            return true;
+        }
+        return false;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+
+    public void addReservation(Reservation reservation) {
+        reservations.add(reservation);
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+}
